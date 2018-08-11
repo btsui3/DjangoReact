a4dcbf93ee958eb60b7b4a9d608bf8307e5fc29e
btsui <btsui4@gmail.com>
2018-08-11 15:42:24 -0600

    added bootstrap

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 //stateless functional component
-const NavBar = ({totalCounters}) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand">
         Navbar{" "}
-        <span className="bade badge-pill badge-secondary">
-          {totalCounters}
-        </span>
+        <span className="bade badge-pill badge-secondary">{totalCounters}</span>
       </a>
     </nav>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
